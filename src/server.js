c5c124d74409531916d88eb383be3e2e7b7f818a
comstudynews <comstudynews@gmail.com>
2022-06-09 13:06:50 +0900

    chapter02 first

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
 import express from "express";
 
 const app = express();
+app.set("port", 3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
-app.listen(3000, ()=> {
-    console.log("listening on http://localhost:3000");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res)=>res.render("home"))
+// app.get("/", (req, res)=>{
+//     req.app.render("home", {}, (err, html)=>{
+//         if(err) throw err;
+//         res.end(html);
+//     })
+// });
+app.get("/*", (req, res)=> res.redirect("/"))
+
+app.listen(app.get("port"), ()=> {
+    console.log(`listening on http://localhost:${app.get("port")}`);
 });
